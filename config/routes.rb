9a6b08a6f02f042bd613e76f5d67db282ce6e7c0
helloasuka <helloasuka@gmail.com>
2018-12-30 21:55:07 +0000

    minor changes to css

--- config/routes.rb
+++ config/routes.rb
@@ -26,3 +26,5 @@ Rails.application.routes.draw do
   get 'exercise3', action: :exercise3, controller: 'exercises'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
